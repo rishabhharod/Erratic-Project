aa0c31c487c6613c1414af837cbc16d96bd4fd7f
rishabh harod <rishabhharod4449@gmail.com>
2024-08-05 17:24:54 +0530

    complete the user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :products
   has_many :orders
   has_one :cart
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :contact, length: {is: 10}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
