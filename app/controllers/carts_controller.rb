f80ce018e72d97250dcb03a62cfca512c1994bb1
Rishabh Harod <rishabhharod4449@gmail.com>
2024-08-23 15:12:54 +0530

    resolve the error

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,10 @@
 class CartsController < ApplicationController
   before_action :set_cart,only: [:show]
+
   def show
     @cart_items = @cart.cart_items
   end
+  
   private 
   def set_cart
     @cart = current_user.cart || current_user.create_cart
