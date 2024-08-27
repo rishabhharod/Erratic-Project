a74b26b7ce5f0450ed812c1fc8b4a79f48517d70
Rishabh Harod <rishabhharod4449@gmail.com>
2024-08-27 17:57:14 +0530

    create wishlist controllers

---
+++ app/controllers/wishlist_items_controller.rb
@@ -0,0 +1,31 @@
+class WishlistItemsController < ApplicationController
+  before_action :set_wishlist
+  def create
+    product = Product.find(params[:product_id])
+    @wishlist_items = @wishlist.wishlist_items.find_or_initialize_by(product_id: product.id)
+    if @wishlist_items.new_record?
+      @wishlist_items.save
+      flash[:notice] = 'Product wishlisted'
+      redirect_to wishlists_show_path
+    else
+      flash[:alert] = 'Already wishlisted'
+      redirect_to product_path(product)
+    end
+  end
+
+  def destroy
+    @wishlist_item = @wishlist.wishlist_items.find(params[:id])
+    if @wishlist_item.destroy
+      flash[:notice] = 'Product removed'
+      redirect_to wishlist_path
+    else
+      flash[:alert] = 'Product id not found'
+      render wishlist: show
+    end
+  end
+
+  private
+  def set_wishlist
+    @wishlist = current_user.wishlist || current_user.create_wishlist
+  end
+end
---
+++ app/controllers/wishlists_controller.rb
@@ -0,0 +1,11 @@
+class WishlistsController < ApplicationController
+  before_action :set_wishlist, only: [:show] 
+  def show
+    @wishlist_items = @wishlist.wishlist_items
+  end
+  
+  private 
+  def set_wishlist
+    @wishlist = current_user.wishlist || current_user.create_wishlist
+  end
+end
