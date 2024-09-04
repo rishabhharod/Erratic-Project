80cce88e30b276283333fd0f169a5e36000dbf97
Rishabh Harod <rishabhharod4449@gmail.com>
2024-09-04 18:59:06 +0530

    added address destroy,edit,update path

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   get "sellerProduct", to: 'products#sellerProduct'
-  resources :addresses, only: [:index,:create,:update]
+  resources :addresses
   resources :carts, only: [:index]
   resources :cart_items, only: [:create,:update,:destroy]
   resources :wishlists, only: [:show]
