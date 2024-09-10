c2a6b9d17b64c7e81f8b56acbbe55b2a7cf54dee
Rishabh Harod <rishabhharod4449@gmail.com>
2024-09-10 13:55:22 +0530

    added required:true in forms

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
+    validates_presence_of : first_name, last_name, birthday, sex,home, message: "This field shouldn't be blank"
     has_many :orders
     belongs_to :order
 end
