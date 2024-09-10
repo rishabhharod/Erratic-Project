918f46dd79b60c4a1342f7dcae1f5c9f5da602bd
Rishabh Harod <rishabhharod4449@gmail.com>
2024-09-10 14:25:36 +0530

    added validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,6 @@
 class Address < ApplicationRecord
-    validates_presence_of : first_name, last_name, birthday, sex,home, message: "This field shouldn't be blank"
+    validates_presence_of :full_address, :state, :city, :pincode, :landmark, :user_id, :address_type
+    validates :pincode, length: {is: 6}
     has_many :orders
     belongs_to :order
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   has_many :addresses
   has_one :cart
   has_one :wishlist
-  has_one_attached :profile
-  validates :name, presence: true
-  validates :gender, presence: true
-  validates :contact, length: {is: 10}
-  validates :role, presence: true
-  enum role: %i[buyer seller admin]
-  after_initialize :set_default_role, if: :new_record?
   has_one :seller
+  has_one_attached :profile
+  validates_presence_of :name, :gender, :contact
+  validates :contact, length: {is: 10}
+
+  after_initialize :set_default_role, if: :new_record?
+  
+  enum role: %i[buyer seller admin]
+
   def set_default_role
     self.role ||= :buyer
   end
